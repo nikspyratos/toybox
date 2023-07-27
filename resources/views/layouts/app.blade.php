3e44bfb13e2987a9b059cdb328dbb391786a1441
Nik Spyratos <17888779+nikspyratos@users.noreply.github.com>
2023-07-27 08:48:52 +0200

    Feat: Laravel Livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @livewireStyles
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -32,5 +33,6 @@
                 {{ $slot }}
             </main>
         </div>
+        @livewireScripts
     </body>
 </html>
