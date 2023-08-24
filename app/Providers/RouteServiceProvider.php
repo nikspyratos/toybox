6c0896325493508f34fb6248ca1d483773b365b4
Nik Spyratos <17888779+nikspyratos@users.noreply.github.com>
2023-08-25 01:08:45 +0200

    Feat: Remove Breeze

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -19,7 +19,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/home';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
