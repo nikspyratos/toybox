b7c3198f7bb3d8f5a3de1bfeeed5ba15d4d051fa
Nik Spyratos <17888779+nikspyratos@users.noreply.github.com>
2023-12-12 16:29:42 +0200

    Feat: Basic activity log viewing for Users (as example for other models)

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -12,60 +12,47 @@
 
 namespace App\Models{
 /**
- * App\Models\Permission
+ * App\Models\Activity
  *
  * @property int $id
- * @property string $name
- * @property string $guard_name
+ * @property string|null $log_name
+ * @property string $description
+ * @property string|null $subject_type
+ * @property int|null $subject_id
+ * @property string|null $causer_type
+ * @property int|null $causer_id
+ * @property \Illuminate\Support\Collection|null $properties
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \Spatie\Permission\Models\Permission> $permissions
- * @property-read int|null $permissions_count
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \Spatie\Permission\Models\Role> $roles
- * @property-read int|null $roles_count
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\User> $users
- * @property-read int|null $users_count
- * @method static \Illuminate\Database\Eloquent\Builder|Permission newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Permission newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Permission permission($permissions, $without = false)
- * @method static \Illuminate\Database\Eloquent\Builder|Permission query()
- * @method static \Illuminate\Database\Eloquent\Builder|Permission role($roles, $guard = null, $without = false)
- * @method static \Illuminate\Database\Eloquent\Builder|Permission whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Permission whereGuardName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Permission whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Permission whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Permission whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Permission withoutPermission($permissions)
- * @method static \Illuminate\Database\Eloquent\Builder|Permission withoutRole($roles, $guard = null)
+ * @property string|null $event
+ * @property string|null $batch_uuid
+ * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent $causer
+ * @property-read \Illuminate\Support\Collection $changes
+ * @property-read string $subject_title
+ * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent $subject
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity causedBy(\Illuminate\Database\Eloquent\Model $causer)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity forBatch(string $batchUuid)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity forEvent(string $event)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity forSubject(\Illuminate\Database\Eloquent\Model $subject)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity hasBatch()
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity inLog(...$logNames)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereBatchUuid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereCauserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereCauserType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereEvent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereLogName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereProperties($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereSubjectId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereSubjectType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Activity whereUpdatedAt($value)
  */
-	class Permission extends \Eloquent {}
-}
-
-namespace App\Models{
-/**
- * App\Models\Role
- *
- * @property int $id
- * @property string $name
- * @property string $guard_name
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \Spatie\Permission\Models\Permission> $permissions
- * @property-read int|null $permissions_count
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\User> $users
- * @property-read int|null $users_count
- * @method static \Illuminate\Database\Eloquent\Builder|Role newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Role newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Role permission($permissions, $without = false)
- * @method static \Illuminate\Database\Eloquent\Builder|Role query()
- * @method static \Illuminate\Database\Eloquent\Builder|Role whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Role whereGuardName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Role whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Role whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Role whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Role withoutPermission($permissions)
- */
-	class Role extends \Eloquent {}
+	class Activity extends \Eloquent {}
 }
 
 namespace App\Models{
@@ -80,20 +67,18 @@ namespace App\Models{
  * @property string|null $remember_token
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \App\Enumerations\Role $role
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Spatie\Activitylog\Models\Activity> $activities
+ * @property-read int|null $activities_count
+ * @property-read bool $is_admin
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
  * @property-read int|null $notifications_count
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \Spatie\Permission\Models\Permission> $permissions
- * @property-read int|null $permissions_count
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \Spatie\Permission\Models\Role> $roles
- * @property-read int|null $roles_count
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
  * @property-read int|null $tokens_count
  * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
  * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|User permission($permissions, $without = false)
  * @method static \Illuminate\Database\Eloquent\Builder|User query()
- * @method static \Illuminate\Database\Eloquent\Builder|User role($roles, $guard = null, $without = false)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
@@ -102,8 +87,6 @@ namespace App\Models{
  * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User withoutPermission($permissions)
- * @method static \Illuminate\Database\Eloquent\Builder|User withoutRole($roles, $guard = null)
  */
 	class User extends \Eloquent implements \Filament\Models\Contracts\FilamentUser, \Filament\Models\Contracts\HasAvatar, \Illuminate\Contracts\Auth\MustVerifyEmail {}
 }
---
+++ app/Filament/Resources/UserResource/RelationManagers/ActivitiesRelationManager.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Resources\UserResource\RelationManagers;
+
+use App\Filament\Resources\UserResource;
+use Filament\Infolists\Components\Grid;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Support\Colors\Color;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use Spatie\Activitylog\Models\Activity;
+
+class ActivitiesRelationManager extends RelationManager
+{
+    protected static ?string $title = 'Event Log';
+    protected static string $relationship = 'activities';
+
+    public static function canViewForRecord(Model $ownerRecord, string $pageClass): bool
+    {
+        return auth()->user()->is_admin;
+    }
+
+    public function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                TextEntry::make('description')
+                    ->formatStateUsing(fn (string $state) => Str::ucfirst($state)),
+                TextEntry::make('causer.fullname')
+                    ->label('Updated By')
+                    ->color(Color::Cyan)
+                    ->url(fn (Activity $activity) => UserResource::getUrl('view', ['record' => $activity->causer])),
+                TextEntry::make('created_at')
+                    ->label('Updated at'),
+                Grid::make(4)
+                    ->schema([
+                        Section::make('Before')
+                            ->columnSpan(2)
+                            ->schema(function (Activity $activity): array {
+                                $schema = [];
+                                $oldValuesThatChanged = array_diff_assoc($activity->properties->get('old'), $activity->properties->get('attributes'));
+                                foreach ($oldValuesThatChanged as $key => $value) {
+                                    $schema[] = TextEntry::make($key)->default($value);
+                                }
+
+                                return $schema;
+                            })->visible(fn (Activity $activity): bool => Arr::has($activity->properties, 'old')),
+                        Section::make(fn (Activity $activity): string => Arr::has($activity->properties, 'old') ? 'After' : '')
+                            ->columnSpan(2)
+                            ->schema(function (Activity $activity): array {
+                                $schema = [];
+                                $newValuesThatChanged = array_diff_assoc($activity->properties->get('attributes'), $activity->properties->get('old') ?? []);
+                                foreach ($newValuesThatChanged as $key => $value) {
+                                    if ($key == 'password') {
+                                        $schema[] = TextEntry::make('Password')->default('Changed');
+                                    } elseif (! in_array($key, ['deleted_at', 'last_login_at'])) {
+                                        $schema[] = TextEntry::make($key)->default($value);
+                                    }
+                                }
+
+                                return $schema;
+                            })->visible(fn (Activity $activity): bool => Arr::has($activity->properties, 'attributes')),
+                    ]),
+
+            ]);
+    }
+
+    public function isReadOnly(): bool
+    {
+        return true;
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('description')
+            ->columns([
+                TextColumn::make('description')
+                    ->label('Action')
+                    ->formatStateUsing(fn (string $state) => Str::ucfirst($state)),
+                TextColumn::make('causer.fullname')
+                    ->label('Updated By')
+                    ->color(Color::Cyan)
+                    ->url(fn (Activity $activity) => UserResource::getUrl('view', ['record' => $activity->causer]))
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->label('Updated at'),
+            ])
+            ->defaultSort('created_at', 'desc')
+            ->filters([
+                //
+            ])
+            ->headerActions([
+            ])
+            ->actions([
+                ViewAction::make()
+                    ->label('View changes'),
+            ])
+            ->bulkActions([
+            ]);
+    }
+}
