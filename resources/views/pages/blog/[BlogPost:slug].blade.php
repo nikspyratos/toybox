4ff266d0ff6abbe1416bfd368937a0ff49f88607
Nik Spyratos <17888779+nikspyratos@users.noreply.github.com>
2023-12-18 23:21:08 +0200

    Feat: Blog post styling
    (cherry picked from commit e64c6c0aaafaf942502744b8085ce87322bc8129)

--- resources/views/pages/blog/[BlogPost:slug].blade.php
+++ resources/views/pages/blog/[BlogPost:slug].blade.php
@@ -6,7 +6,16 @@
 @extends('layouts.marketing')
 
 @section('content')
-<div>
-    {{ $blogPost->title }}
-</div>
+<section class="bg-gray-50 dark:bg-gray-800">
+    <div class="px-4 pt-8 pb-4 mx-auto w-3/4 max-w-screen-xl lg:pt-24">
+        <h1 class="mb-6 text-4xl font-extrabold tracking-tight leading-none text-center md:text-5xl xl:text-6xl dark:text-white">{{ $blogPost->title }}</h1>
+        <div class="p-6 mx-auto mb-6 text-gray-900 bg-white xl:p-8 dark:text-white dark:bg-gray-800 dark:border-gray-600">
+                <span class="text-matisse-500">{{ $blogPost->published_at->toDateString() }}</span>
+                <div class="mt-4">
+                    {{-- TOC generation --}}
+                    {!! $blogPost->content !!}
+                </div>
+        </div>
+    </div>
+</section>
 @endsection
