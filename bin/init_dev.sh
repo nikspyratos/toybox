64fa3276dee6888b666dc16d6fa5c675f0eb7ddf
Nik Spyratos <17888779+nikspyratos@users.noreply.github.com>
2023-12-27 16:53:53 +0200

    Fix: Duster pre-commit lint command

--- bin/init_dev.sh
+++ bin/init_dev.sh
@@ -23,7 +23,7 @@ sed -i.bak "s/APP_PATH/$prod_app_path/g" Caddyfile
 sed -i.bak "s/APP_PATH/$prod_app_path/g" templates/octane.conf
 if [[ $strict_hooks == *"y"* ]]; then
   sed -i.bak "s/#strict_e_placeholder/set -e/g" ./.hooks/pre-commit
-  sed -i.bak "s/#strict_lint_placeholder/duster lint --dirty/g" ./.hooks/pre-commit
+  sed -i.bak "s/#strict_lint_placeholder/vendor\/bin\/duster lint --dirty/g" ./.hooks/pre-commit
 fi
 # Local setup
 git config --local include.path ../.gitconfig
