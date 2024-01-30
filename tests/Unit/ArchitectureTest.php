d5b7a271222ff731cc17d9afb7a86432ba7f8036
Nik Spyratos <17888779+nikspyratos@users.noreply.github.com>
2024-01-30 21:45:51 +0200

    Feat: arch test updates

--- tests/Unit/ArchitectureTest.php
+++ tests/Unit/ArchitectureTest.php
@@ -8,51 +8,55 @@ declare(strict_types=1);
  */
 uses()->group('architecture');
 
-test('Application files use strict types')
+arch('Application files use strict types')
     ->expect('App')
     ->toUseStrictTypes();
 
-test('The codebase does not reference env variables outside of config files')
+arch('The codebase does not reference env variables outside of config files')
     ->expect('env')
     ->not->toBeUsed();
 
-test('The codebase does not contain any debugging code')
+arch('The codebase does not contain any debugging code')
     ->expect(['dd', 'dump', 'ray', 'var_dump', 'print_r'])
     ->not->toBeUsed();
 
-test('Mailables are extending the correct class')
+arch('Session data is not accessed in async jobs')
+    ->expect(['session', 'auth', 'request'])
+    ->each->not->toBeUsedIn(['App\Jobs', 'App\Console']);
+
+arch('Mailables are extending the correct class')
     ->expect('App\Mail')
     ->toBeClasses()
 //    ->classes->toBeFinal() // optional
     ->classes->toExtend('Illuminate\Mail\Mailable');
 //    ->classes->toImplement('Illuminate\Contracts\Queue\ShouldQueue'); // optional
 
-test('Models are extending the correct class')
+arch('Models are extending the correct class')
     ->expect('App\Models')
     ->toBeClasses()
 //    ->classes->toBeFinal() // optional
     ->classes->toExtend('Illuminate\Database\Eloquent\Model');
 
-test('Providers are extending the correct class')
+arch('Providers are extending the correct class')
     ->expect('App\Providers')
     ->toBeClasses()
 //    ->classes->toBeFinal() // optional
     ->classes->toExtend('Illuminate\Support\ServiceProvider')
     ->classes->toImplementNothing();
 
-test('Requests are extending the correct class')
+arch('Requests are extending the correct class')
     ->expect('App\Http\Requests')
     ->toBeClasses()
 //    ->classes->toBeFinal() // optional
     ->classes->toExtend('Illuminate\Foundation\Http\FormRequest');
 
-test('Commands are extending the correct class')
+arch('Commands are extending the correct class')
     ->expect('App\Console\Commands')
     ->toBeClasses()
 //    ->classes->toBeFinal() // optional
     ->classes->toExtend('Illuminate\Console\Command');
 
-test('Tests are using strict types and have the correct suffix')
+arch('Tests are using strict types and have the correct suffix')
     ->expect('Tests')
     ->toUseStrictTypes()
     ->and('Tests\Feature')->toHaveSuffix('Test')
