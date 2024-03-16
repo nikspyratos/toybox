bbcf8c4207f1913e6439b570550c054149077532
Shift <shift@laravelshift.com>
2024-03-16 06:35:02 +0000

    Set new `ENV` variables

--- config/database.php
+++ config/database.php
@@ -9,7 +9,7 @@ return [
     'connections' => [
         'pulse' => [
             'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
+            'url' => env('DB_URL'),
             'database' => env('PULSE_DATABASE', database_path('pulse.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
