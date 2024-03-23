f8329d084e930baf325832d4d4abb4358479ecc1
Nik Spyratos <17888779+nikspyratos@users.noreply.github.com>
2024-03-23 17:08:56 +0200

    Feat: Remove unused/superfluous routes

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,6 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-//Route::view('dashboard', 'dashboard')
-//    ->middleware(['auth', 'verified'])
-//    ->name('dashboard');
-//
-//Route::view('profile', 'profile')
-//    ->middleware(['auth'])
-//    ->name('profile');
-
 require __DIR__ . '/auth.php';
 
 Route::middleware([
