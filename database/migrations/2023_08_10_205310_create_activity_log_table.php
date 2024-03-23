6063e993696f0e7249b7a5127ee17396f3343f1d
Nik Spyratos <17888779+nikspyratos@users.noreply.github.com>
2024-03-23 15:27:14 +0200

    Feat: Consolidate activity log migrations

--- database/migrations/2023_08_10_205310_create_activity_log_table.php
+++ database/migrations/2023_08_10_205310_create_activity_log_table.php
@@ -20,8 +20,10 @@ return new class extends Migration
                     $table->string('log_name')->nullable();
                     $table->text('description');
                     $table->nullableMorphs('subject', 'subject');
+                    $table->string('event')->nullable();
                     $table->nullableMorphs('causer', 'causer');
                     $table->json('properties')->nullable();
+                    $table->uuid('batch_uuid')->nullable();
                     $table->timestamps();
                     $table->index('log_name');
                 }
