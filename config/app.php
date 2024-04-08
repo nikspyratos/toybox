b806893f4d9c56188771798bec632dfce430e7dc
Nik Spyratos <17888779+nikspyratos@users.noreply.github.com>
2024-04-08 17:46:16 +0200

    More errant deletions

--- config/app.php
+++ config/app.php
@@ -129,11 +129,8 @@ return [
 
     'contact-email' => env('CONTACT_EMAIL'),
 
-    //https://developers.google.com/search/docs/advanced/robots/robots_meta_tag
     'default_robots' => 'max-snippet:-1,max-image-preview:large,max-video-preview:-1',
 
     'default_description' => 'A TALL stack starter kit for solopreneurs',
-
-    'coming_soon_enabled' => env('COMING_SOON_ENABLED', false),
 
 ];
