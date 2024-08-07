173c1869f3cd2a61f325c83d6be1ad59b0d1070f
Nik Spyratos <17888779+nikspyratos@users.noreply.github.com>
2024-08-07 21:02:29 +0200

    Cookie consent config

---
+++ lang/vendor/cookie-consent/nb/texts.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'message' => 'Din opplevelse på dette nettstedet vil bli forbedret ved å tillate informasjonskapsler (cookies).',
+    'agree' => 'Tillat informasjonskapsler',
+];
